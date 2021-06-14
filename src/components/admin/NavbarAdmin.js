ec072c7b0c6639ab484870357bd4b32587b95d18
Cristian Galluccio <gallucciocristian@gmail.com>
2021-06-14 15:13:08 -0300

    handleImageChange and handleVideoChange added

--- src/components/admin/NavbarAdmin.js
+++ src/components/admin/NavbarAdmin.js
@@ -10,18 +10,14 @@ export const NavbarAdmin = () => {
                 <div className="container-fluid">
                     <Logo style={{margin: '0px !important'}} />
                     <form className="d-flex">
-                        {/* <button className="btn btn-custom-primary shadow-none" type="submit">
-                            <i className="fas fa-sign-out-alt"></i>
-                            <span>   Logout</span>
-                        </button> */}
                         <Link to="/" >
                             <button className="btn shadow-none admin-button">
-                                <i class="fas fa-home"></i>
+                                <i className="fas fa-home"></i>
                             </button>
                         </Link>
                         <Link to="/login" >
                             <button className="btn shadow-none admin-button">
-                                <i class="fas fa-sign-out-alt"></i>
+                                <i className="fas fa-sign-out-alt"></i>
                             </button>
                         </Link>
                     </form>
--- src/components/admin/Upload.js
+++ src/components/admin/Upload.js
@@ -1,35 +1,46 @@
 import React from 'react';
 
 export const Upload = () => {
+
+    const handleImageChange = (e) => {
+        const image = e.target.files[0];
+        console.log(image);
+    }
+
+    const handleVideoChange = (e) => {
+        const video = e.target.files[1];
+        console.log(video);
+    }
+ 
     return (
-        <div>
+        <>
             <div className="col-lg-12">
                 <div className="jumbotron jumbotron-fluid text-center mt-5">
                     <div className="container">
                         <div className="row justify-content-center">
                             <div className="upload-content">
                                 <div className="form-group upload-input">
-                                    <input type="text" className="form-control shadow-none upload-input-title" name="title" placeholder="Título" maxlength="30" autoComplete="off" />
-                                    <input type="number" className="form-control shadow-none upload-input-year" name="year" placeholder="Año" autoComplete="off" />
-                                    <select class="form-select shadow-none upload-select-gender" aria-label="Default select example" placeholder="Seleccione un genero">
-                                        <option value="" selected disabled>Seleccione un género</option>
+                                    <input type="text" className="form-control shadow-none upload-input-title" name="title" placeholder="Título" maxLength="30" autoComplete="off" />
+                                    <input type="number" className="form-control shadow-none upload-input-year" name="year" placeholder="Año" autoComplete="off"/>
+                                    <select className="form-select shadow-none upload-select-gender" aria-label="Default select example" placeholder="Seleccione un genero">
+                                        <option value="" disabled>Seleccione un género</option>
                                     </select>
-                                    <textarea className="form-control upload-input-sinopsis" placeholder="Sinópsis" maxlength="500" />
+                                    <textarea className="form-control upload-input-sinopsis" placeholder="Sinópsis" maxLength="500" />
                                     <label className="mt-4">Subir imagen</label>
-                                    <div class="upload-image">
-                                        <label for="file-input">
-                                            <i class="fas fa-image fa-3x upload-image-icon"></i>
+                                    <div className="upload-image">
+                                        <label htmlFor="file-input">
+                                            <i className="fas fa-image fa-3x upload-image-icon"></i>
                                         </label>
 
-                                        <input id="file-input" type="file" />
+                                        <input id="file-input" type="file" onChange={handleImageChange}/>
                                     </div>
                                     <label className="mt-4">Subir video</label>
-                                    <div class="upload-video">
-                                        <label for="file-input">
-                                            <i class="fas fa-film fa-3x upload-video-icon"></i>
+                                    <div className="upload-video">
+                                        <label htmlFor="file-input">
+                                            <i className="fas fa-film fa-3x upload-video-icon"></i>
                                         </label>
 
-                                        <input id="file-input" type="file" />
+                                        <input id="file-input" type="file" onChange={handleVideoChange}/>
                                     </div>
                                     <button className="btn shadow-none upload-button" >Subir</button>
                                 </div>
@@ -38,6 +49,6 @@ export const Upload = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
